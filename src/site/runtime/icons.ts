505dbf577af584b4c27d9046700e519df6567d04
Marco Ieni <11428655+marcoieni@users.noreply.github.com>
2026-05-13 23:02:45 +0200

    make PR collapsable (#23)

---
+++ src/site/runtime/icons.ts
@@ -0,0 +1,2 @@
+export const collapseChevronSvg =
+    '<svg aria-hidden="true" viewBox="0 0 16 16" width="16" height="16"><path d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"/></svg>';
