a12eba7bab4ef83f87c4998ed6666275cd368590
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2026-05-13 23:01:35 +0200

    make PR collapsable

--- src/site/runtime/diffs.ts
+++ src/site/runtime/diffs.ts
@@ -6,6 +6,7 @@ import {
     type FileDiffMetadata,
     type ThemeTypes,
 } from "@pierre/diffs";
+import { collapseChevronSvg } from "./icons.js";
 
 const diffs: FileDiff<ReviewAnnotation>[] = [];
 const currentTheme = getCurrentTheme();
@@ -120,7 +121,7 @@ function createCollapseButton({
     button.className = "diff-collapse-toggle";
     button.type = "button";
     updateCollapseButtonState(button, container.hasAttribute("data-collapsed"));
-    button.innerHTML = `<svg aria-hidden="true" viewBox="0 0 16 16" width="16" height="16"><path d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z"/></svg>`;
+    button.innerHTML = collapseChevronSvg;
 
     button.addEventListener("click", () => {
         const collapsed = !container.hasAttribute("data-collapsed");
